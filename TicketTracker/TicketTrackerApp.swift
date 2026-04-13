765b7937e1af5e7d1e43c0412776a63df58586a1
PabloR4M <ramospablofrancisco@gmail.com>
2026-04-13 10:46:03 -0700

    Initial Commit

---
+++ TicketTracker/TicketTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketTrackerApp.swift
+//  TicketTracker
+//
+//  Created by Pablo on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TicketTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
